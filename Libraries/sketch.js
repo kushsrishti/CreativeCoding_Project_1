5d11f27d9c85c1c213aa395ad5e6d8440267c235
kushsrishti <32043930+kushsrishti@users.noreply.github.com>
2017-10-29 19:57:52 -0400

    Calm2.0

--- Libraries/sketch.js
+++ Libraries/sketch.js
@@ -1,32 +1,61 @@
-function setup() {
-    createCanvas(windowWidth, windowHeight); 
-WW = windowWidth/500
-WH = windowHeight/2
+
 cnt = 0
 mult = 60
 diff = 0.02
+
+function setup() {
+createCanvas(windowWidth, windowHeight); 
+WW = windowWidth/500
+WH = windowHeight/10
 } 
-function draw() {
-background(135,206,250); 
+function draw() 
+
+{
+
+  
+  
+  if (mouseIsPressed)
+    background(255);
+  else
+     background(135,206,250); 
+  everything();
+clouds();
+
+}
+
+function everything()
+{
+
 noStroke();
-fill(212,112,100,200)
+fill(212,112,100)
 cnt = cnt + 0.02
 beginShape();
 for(var i=0; i<501; i++){
-vertex(WW*i, sin(cnt-diff*i)*mult+WH);
+vertex(WW*i, sin(cnt-mult*i)*diff+WH);
 }
-vertex(WW*500, WH*2);
-vertex(WW*0, WH*2);
+vertex(WH*50, WW*0);
+vertex(WH*0, WW*2);
 endShape(CLOSE);
-frameRate(5);  
-if (mouseY)
-  mult = mult + 5
-if (mouseX)
-  mult = mult - 0.1
+frameRate(0.5);
+  
+if(diff=10)
+  mult = mult + 15
+if ( diff=200)
+  mult = mult - 1
   
 if (mouseY)
   diff = diff + 4
 if (mouseX)
-  diff = diff - 0.002
+  diff = diff - 0.02
+
 }
 
+function clouds() {
+  
+
+  noStroke();
+    fill (202,220,255);
+     arc (100, 100, 60, 90, PI, PI);
+     arc (130, 110, 60, 60, PI, TWO_PI);
+     arc (80,110, 60, 60, PI, TWO_PI);
+}
